b5343b138b4b156a83d1c7cd688389acb4071e7a
AbhishekNagdeve <abhishek.nagdeve@gmail.com>
2024-10-15 11:50:51 +0530

    chore: set up basic django project

---
+++ authores_api/settings/production.py
@@ -0,0 +1,7 @@
+from .base import * #noqa
+from .base import env
+
+ADMINS = [("Abhishek Nagdeve" , "abhishek.nagdeve@gmail.com")]
+
+#TODO add Domain names of production server
+CSRF_TRUSTED_ORIGINS = []
